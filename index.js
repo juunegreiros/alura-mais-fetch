00152abd1afea5021e19d27fa152ad11d829e20c
Ju <ju_bsnegreiros@hotmail.com>
2019-08-05 13:50:20 -0300

    Adiciona catch

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const apiUrl = 'http://localhost:5000/clientes'
-const clientes = [{nome: 'teste', cpf: '00000000000'}]
 const listaClientes = document.querySelector('#listaClientes')
 
 fetch(apiUrl)
   .then(resposta => resposta.json())
-  .then(clientesApi => clientesApi.forEach(cliente => clientes.push({
-    nome: cliente.nome,
-    cpf: cliente.cpf
-  })))
-
-  clientes.forEach(cliente => listaClientes.innerHTML += `<li>nome: ${cliente.nome} cpf: ${cliente.cpf} </li>`)
+  .then(clientesApi => clientesApi.forEach(cliente => 
+    listaClientes.innerHTML += `<li>nome: ${cliente.nome} cpf: ${cliente.cpf} </li>`)
+  )
+  .catch(erro => listaClientes.innerHTML = `<p>Erro :(</p>`)
