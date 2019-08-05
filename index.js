b58b0db284aa7e29806a392b09334c50f0dc30f3
Ju <ju_bsnegreiros@hotmail.com>
2019-08-05 13:43:14 -0300

    Adiciona branch onde o vídeo irá começar

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const apiUrl = 'http://localhost:5000/clientes'
 const clientes = [{nome: 'teste', cpf: '00000000000'}]
 const listaClientes = document.querySelector('#listaClientes')
 
-fetch(apiUrl)
-  .then(resposta => resposta.json())
-  .then(clientesApi => clientesApi.forEach(cliente => clientes.push({
-    nome: cliente.nome,
-    cpf: cliente.cpf
-  })))
-
-  clientes.forEach(cliente => listaClientes.innerHTML += `<li>nome: ${cliente.nome} cpf: ${cliente.cpf} </li>`)
+clientes.forEach(cliente => listaClientes.innerHTML += `<li>nome: ${cliente.nome} cpf: ${cliente.cpf} </li>`)
